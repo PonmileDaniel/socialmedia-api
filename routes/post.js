61bd83d60312e72ea0b022cb7db4c3eb877054b8
Ndukwe Daniel <danielndukwe11@gmail.com>
2023-05-29 17:27:21 -0700

    More Modification

--- routes/post.js
+++ routes/post.js
@@ -1,6 +1,6 @@
 const router = require("express").Router();
-const Post = require("../../api/models/Post");
-const User = require("../../api/models/User")
+const Post = require("../models/Post");
+const User = require("../models/User")
 //Create a Post
 router.post("/", async(req,res) => {
  const newPost = new Post (req.body);
